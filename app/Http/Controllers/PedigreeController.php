710d3261649ea336136fd0cde96094218ac3110e
Curtis Delicata <curtis.delicata@outlook.com>
2020-11-29 17:00:47 +0000

    Pedigree beginnings and update to TreeController.php

--- app/Http/Controllers/PedigreeController.php
+++ app/Http/Controllers/PedigreeController.php
@@ -3,68 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Models\Family;
+use App\Jobs\ExportGedCom;
+use App\Models\Note;
 use App\Models\Person;
-use Illuminate\Http\Request;
-use App\Models\Tree;
+use App\Models\User;
+use LaravelEnso\Core\Models\User as CoreUser;
+// use LaravelEnso\Avatars\Models\Avatar;
+// use LaravelEnso\Files\Models\File as UploadFile;
 
-class TreeController extends Controller
+// use App\Traits\ConnectionTrait;
+use File;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use GenealogiaWebsite\LaravelGedcom\Utils\GedcomGenerator;
+use Response;
+
+class PedigreeController extends Controller
 {
 
     private $persons;
     private $unions;
     private $links;
+    private $all;
     private $nest;
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return datatables()->of(Tree::query())->toJson();
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required',
-            'description' => 'required',
-            'user_id' => 'required'
-        ]);
-
-        return Tree::create([
-            'name' => $request->adr1,
-            'description' => $request->adr2,
-            'user_id' => $request->city
-        ]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(Request $request)
     {
-
         $start_id = $request->get('start_id', 3);
         $nest = $request->get('nest', 3);
         $ret = [];
@@ -78,6 +44,7 @@ class TreeController extends Controller
         $ret['persons'] = $this->persons;
         $ret['unions'] = $this->unions;
         $ret['links'] = $this->links;
+        $ret['all'] = $this->all;
         // ExportGedCom::dispatch(2, $request);
         // $file = 'file.GED';
         // $destinationPath = public_path().'/upload/';
@@ -86,85 +53,7 @@ class TreeController extends Controller
         return $ret;
     }
 
-        private function getGraphData($start_id, $nest = 1)
-    {
-
-        $conn = $this->getConnection();
-        $db = $this->getDB();
-
-        if ($this->nest >= $nest) {
-            $nest++;
-
-            // add family
-            $families = Family::on($conn)->where('husband_id', $start_id)->orwhere('wife_id', $start_id)->get();
-            $own_unions = [];
-
-            // add children
-            foreach ($families as $family) {
-                $family_id = $family->id;
-                $father = Person::on($conn)->find($family->husband_id);
-                $mother = Person::on($conn)->find($family->wife_id);
-                // add partner to person
-                // add partner link
-
-                if (isset($father->id)) {
-                    $_families = Family::on($conn)->where('husband_id', $father->id)->orwhere('wife_id', $father->id)->select('id')->get();
-                    $_union_ids = [];
-                    foreach ($_families as $item) {
-                        $_union_ids[] = 'u'.$item->id;
-                    }
-                    $father->setAttribute('own_unions', $_union_ids);
-                    $this->persons[$father->id] = $father;
-                    $this->links[] = [$father->id, 'u'.$family_id];
-                }
-                if (isset($mother->id)) {
-                    $_families = Family::on($conn)->where('husband_id', $mother->id)->orwhere('wife_id', $mother->id)->select('id')->get();
-                    $_union_ids = [];
-                    foreach ($_families as $item) {
-                        $_union_ids[] = $item->id;
-                    }
-                    $mother->setAttribute('own_unions', $_union_ids);
-                    $this->persons[$mother->id] = $mother;
-                    $this->links[] = [$mother->id, 'u'.$family_id];
-                }
-
-                // find children
-                $children = Person::on($conn)->where('child_in_family_id', $family_id)->get();
-                $children_ids = [];
-                foreach ($children as $child) {
-                    $child_id = $child->id;
-                    // add child to person
-                    // parent_union
-                    $child_data = Person::on($conn)->find($child_id);
-                    $_families = Family::on($conn)->where('husband_id', $child_id)->orwhere('wife_id', $child_id)->select('id')->get();
-                    $_union_ids = [];
-                    foreach ($_families as $item) {
-                        $_union_ids[] = $item->id;
-                    }
-                    $child_data->setAttribute('own_unions', $_union_ids);
-                    $this->persons[$child_id] = $child_data;
-
-                    // add union-child link
-                    $this->links[] = ['u'.$family_id, $child_id];
-
-                    // make union child filds
-                    $children_ids[] = $child_id;
-                    $this->getGraphData($child_id, $nest);
-                }
-
-                // compose union item and add to unions
-                $union = [];
-                $union['id'] = 'u'.$family_id;
-                $union['partner'] = [isset($father->id) ? $father->id : null, isset($mother->id) ? $mother->id : null];
-                $union['children'] = $children_ids;
-                $this->unions['u'.$family_id] = $union;
-            }
-        }
-
-        return true;
-    }
-
-        private function getGraphDataUpward($start_id, $nest = 0)
+    private function getGraphDataUpward($start_id, $nest = 0)
     {
         $conn = $this->getConnection();
         $db = $this->getDB();
@@ -173,6 +62,21 @@ class TreeController extends Controller
         $has = (int) ($nest) * 1;
         if ($threshold >= $has) {
             $person = Person::on($conn)->find($start_id);
+            if ($person) {
+                $user = $person->user;
+
+                if($user) {
+                    $av = Avatar::where('user_id', '=', $user->id)->first();
+                    $file = UploadFile::where('attachable_id', '=', $av->id)->where('attachable_type', '=', 'avatar')->first();
+
+                    if ($file) {
+                        $person->setAttribute('avatar', '/api/core/avatars/'.$av->id);
+                    } else {
+                        $person->setAttribute('avatar', '');
+                    }
+                }
+            }
+
             // do not process for null
             if ($person == null) {
                 return;
@@ -221,6 +125,37 @@ class TreeController extends Controller
                 $family_id = $family->id;
                 $father = Person::on($conn)->find($family->husband_id);
                 $mother = Person::on($conn)->find($family->wife_id);
+
+                $user = $father->user;
+
+                if($user) {
+                    $av = Avatar::where('user_id', '=', $user->id)->first();
+                    $file = UploadFile::where('attachable_id', '=', $av->id)->where('attachable_type', '=', 'avatar')->first();
+
+                    if ($file) {
+                        $father->setAttribute('avatar', '/api/core/avatars/'.$av->id);
+                    } else {
+                        $father->setAttribute('avatar', '');
+                    }
+                    $father->setAttribute('relation', 'father');
+                }
+
+                if ($mother) {
+                    $user = $mother->user;
+
+                    if($user) {
+                        $av = Avatar::where('user_id', '=', $user->id)->first();
+                        $file = UploadFile::where('attachable_id', '=', $av->id)->where('attachable_type', '=', 'avatar')->first();
+
+                        if ($file) {
+                            $mother->setAttribute('avatar', '/api/core/avatars/'.$av->id);
+                        } else {
+                            $mother->setAttribute('avatar', '');
+                        }
+                        $mother->setAttribute('relation', 'mother');
+                    }
+                }
+
                 if (isset($father->id)) {
                     if (! array_key_exists($father->id, $this->persons)) {
                         // this is not added
@@ -306,67 +241,15 @@ class TreeController extends Controller
                 $this->unions['u'.$family_id] = $union;
             }
             // get brother/sisters
-            $brothers = Person::on($conn)->where('child_in_family_id', $person->child_in_family_id)
-                ->whereNotNull('child_in_family_id')
-                ->where('id', '<>', $start_id)->get();
-            // $nest = $nest -1;
-            foreach ($brothers as $brother) {
-                $this->getGraphDataUpward($brother->id, $nest);
-            }
+            // $brothers = Person::on($conn)->where('child_in_family_id', $person->child_in_family_id)
+            //     ->whereNotNull('child_in_family_id')
+            //     ->where('id', '<>', $start_id)->get();
+            // // $nest = $nest -1;
+            // foreach ($brothers as $brother) {
+            //     $this->getGraphDataUpward($brother->id, $nest);
+            // }
         } else {
             return;
         }
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $request->validate([
-            'name' => 'required',
-            'description' => 'required',
-            'user_id' => 'required'
-        ]);
-
-        $tree = Tree::find($id);
-        $tree->name = $request->name;
-        $tree->description = $request->description;
-        $tree->user_id = $request->user_id;
-        $tree->save();
-        return $tree;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $tree = Tree::find($id);
-        if($tree) {
-            $tree->delete();
-            return "true";
-        }
-        return "false";
-    }
-
-
 }
