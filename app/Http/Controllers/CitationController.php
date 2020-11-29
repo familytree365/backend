ab936af906d292528cb4d3ec82a64dca4e049bbe
Curtis Delicata <curtis.delicata@outlook.com>
2020-11-29 15:38:38 +0000

    Columns for citation.

--- app/Http/Controllers/CitationController.php
+++ app/Http/Controllers/CitationController.php
@@ -36,11 +36,25 @@ class CitationController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required'
+            'name' => 'required',
+            'description' => 'required',
+            'repository_id' => 'required',
+            'volume' => 'required',
+            'page' => 'required',
+            'is_active' => 'required',
+            'confidence' => 'required',
+            'source_id' => 'required'
         ]);
 
         return Citation::create([
-            'name' => $request->name
+            'name' => $request->name,
+            'description' => $request->description,
+            'repository_id' => $request->repository_id,
+            'volume' => $request->volume,
+            'page' => $request->page,
+            'is_active' => $request->is_active,
+            'confidence' => $request->confidence,
+            'source_id' => $request->source_id
         ]);
     }
 
@@ -76,11 +90,25 @@ class CitationController extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
-            'name' => 'required'
-        ]);
+            'name' => 'required',
+            'description' => 'required',
+            'repository_id' => 'required',
+            'volume' => 'required',
+            'page' => 'required',
+            'is_active' => 'required',
+            'confidence' => 'required',
+            'source_id' => 'required'
+       ]);
 
         $citation = Citation::find($id);
         $citation->name = $request->name;
+        $citation->description = $request->description;
+        $citation->repository_id = $request->repository_id;
+        $citation->volume = $request->volume;
+        $citation->page = $request->page;
+        $citation->is_active = $request->is_active;
+        $citation->confidence = $request->confidence;
+        $citation->source_id = $request->source_id;
         $citation->save();
         return $citation;
     }
