e67f4db1bde348bd38b79cc308d7a3c7693f6969
Curtis Delicata <curtis.delicata@outlook.com>
2020-12-02 01:30:56 +0000

    Update data tables usage.

--- app/Http/Controllers/MediaObjectController.php
+++ app/Http/Controllers/MediaObjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MediaObjectFile;
 use Illuminate\Http\Request;
 use App\Models\MediaObject;
 
@@ -12,9 +13,42 @@ class MediaObjectController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return datatables()->of(MediaObject::query())->toJson();
+        $query = MediaObjectFile::query();
+
+        if($request->has('searchTerm')) {
+            $columnsToSearch = ['name', 'email', 'phone'];
+            $search_term = json_decode($request->searchTerm)->searchTerm;
+            if(!empty($search_term)) {
+                $searchQuery = '%' . $search_term . '%';
+                foreach($columnsToSearch as $column) {
+                    $query->orWhere($column, 'LIKE', $searchQuery);
+                }
+            }
+        }
+
+        if($request->has('columnFilters')) {
+
+            $filters = get_object_vars(json_decode($request->columnFilters));
+
+            foreach($filters as $key => $value) {
+                if(!empty($value)) {
+                    $query->orWhere($key, 'like', '%' . $value . '%');
+                }
+            }
+        }
+
+        if($request->has('sort.0')) {
+            $sort = json_decode($request->sort[0]);
+            $query->orderBy($sort->field, $sort->type);
+        }
+
+        if($request->has("perPage")) {
+            $rows = $query->paginate($request->perPage);
+        }
+
+        return $rows;
     }
 
     /**
