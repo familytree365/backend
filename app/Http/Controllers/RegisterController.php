1debf9064c12e46f5f097c465b414f16ff8a4215
biholaindrasinh <biholaindrasinh@gmail.com>
2021-01-08 16:54:56 +0530

    add DatabaseTransaction

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,55 +8,71 @@ use App\Models\User;
 use App\Models\Tree;
 use DB;
 use Artisan;
+use Exception;
 
 class RegisterController extends Controller
 {
     use UsesLandlordConnection;
 
-    public function register(Request $request) {
-    	$request->validate([
-    		'first_name' => ['required'],
+    public function register(Request $request)
+     {
+
+        $request->validate([
+            'first_name' => ['required'],
             'last_name' => ['required'],
-    		'email' => ['required', 'email', 'unique:landlord.users'],
-    		'password' => ['required', 'min:8', 'confirmed']
-    	]);
-
-        $user_id = DB::connection($this->getConnectionName())->table('users')->insertGetId([ 
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password)
+            'email' => ['required', 'email', 'unique:landlord.users'],
+            'password' => ['required', 'min:8', 'confirmed']
         ]);
 
-        $user = User::find($user_id);
-        $user->assignRole('free');
+        DB::connection($this->getConnectionName())->beginTransaction();
 
-        $company_id = DB::connection($this->getConnectionName())->table('companies')->insertGetId([
-            'name' => 'company' . $user_id,
-            'status' => 1,
-            'current_tenant' => 1
-        ]);
+        try
+        {
 
-        DB::connection($this->getConnectionName())->table('user_company')->insert([
-            'user_id' => $user_id,
-            'company_id' => $company_id 
-        ]);
+            $user_id = DB::connection($this->getConnectionName())->table('users')->insertGetId([
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+            ]);
 
-        $tree_id = DB::connection($this->getConnectionName())->table('trees')->insertGetId([
-            'company_id' => $company_id,
-            'name' => 'tree' . $company_id,
-            'description' => '',
-            'current_tenant' => 1
-        ]);
+            $user = User::find($user_id);
+            $user->assignRole('free');
 
-        $tenant_id = DB::connection($this->getConnectionName())->table('tenants')->insertGetId([
-            'name' => 'tenant'.$tree_id,
-            'tree_id' => $tree_id,
-            'database' => 'tenant'.$tree_id
-        ]);
+            $company_id = DB::connection($this->getConnectionName())->table('companies')->insertGetId([
+                'name' => 'company' . $user_id,
+                'status' => 1,
+                'current_tenant' => 1
+            ]);
 
-        DB::statement('create database tenant'.$tree_id);
+            DB::connection($this->getConnectionName())->table('user_company')->insert([
+                'user_id' => $user_id,
+                'company_id' => $company_id
+            ]);
 
-        Artisan::call('tenants:artisan "migrate --database=tenant --force"');
+            $tree_id = DB::connection($this->getConnectionName())->table('trees')->insertGetId([
+                'company_id' => $company_id,
+                'name' => 'tree' . $company_id,
+                'description' => '',
+                'current_tenant' => 1
+            ]);
+
+            $tenant_id = DB::connection($this->getConnectionName())->table('tenants')->insertGetId([
+                'name' => 'tenant'.$tree_id,
+                'tree_id' => $tree_id,
+                'database' => 'tenant'.$tree_id
+            ]);
+
+            DB::statement('create database tenant'.$tree_id);
+
+            Artisan::call('tenants:artisan "migrate --database=tenant --force"');
+        }
+
+        catch(Exception $e)
+        {
+            DB::connection($this->getConnectionName())->rollback();
+        }
+
+        DB::connection($this->getConnectionName())->commit();
     }
 }
