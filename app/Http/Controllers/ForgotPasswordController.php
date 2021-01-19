0ae76ee7ae9872b2a5fc58400492b6e939fe1bd4
pintukalasva <pintu1997.kalasva@gmail.com>
2021-01-19 18:09:03 +0530

    forgot password changes.

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -5,33 +5,67 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ResetPasswordRequest;
 use Illuminate\Support\Facades\Password;
+use DB;
+use Auth;
+use Hash;
+use Carbon\Carbon;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Spatie\Multitenancy\Models\Concerns\UsesLandlordConnection;
 
 class ForgotPasswordController extends Controller
 {
-    public function forgot() {
-        $credentials = request()->validate(['email' => 'required|email']);
+    use UsesLandlordConnection;
 
-        Password::sendResetLink($credentials);
+    public function forgot(Request $request) {
+        $credentials = request()->validate(['email' => 'required|email']);
+        $user = User::where ('email', $request->email)->first();
+        if ( !$user ) return response()->json(['error' => '404','error_msg' => 'Email not exits in record']);
+        $token = $this->generateToken();
+        DB::connection($this->getConnectionName())->table('password_resets')->insert([
+            'email' => $request->email,
+            'token' => $token, //change 60 to any length you want
+            'created_at' => Carbon::now()
+        ]);                              
+        $user->sendPasswordResetNotification($token);
 
         return response()->json(["msg" => 'Reset password link sent on your email id.']);
     }
 
-    public function reset() {
+    private function generateToken()
+    {
+        // This is set in the .env file
+            $key = config('app.key');
+
+        // Illuminate\Support\Str;
+        if (Str::startsWith($key, 'base64:')) {
+            $key = base64_decode(substr($key, 7));
+        }
+        return hash_hmac('sha256', Str::random(40), $key);
+    }
+    
+
+    public function reset(Request $request) {
         $credentials = request()->validate([
             'email' => 'required|email',
             'token' => 'required|string',
             'password' => 'required|string|confirmed'
         ]);
+        $tokenData = DB::connection($this->getConnectionName())->table('password_resets')
+        ->where('token', $request->token)->where('email',$request->email)->first();
+        if($tokenData) {
+            $user = User::where('email', $request->email)->first();
+            if($user) {
+                $user->password = bcrypt($request->password);
+                $user->save(); //or $user->save();
+                DB::connection($this->getConnectionName())->table('password_resets')->where('email', $user->email)->delete();
+                return response()->json(["msg" => 'Password has been successfully changed.']);
 
-        $reset_password_status = Password::reset($credentials, function ($user, $password) {
-            $user->password = bcrypt($password);
-            $user->save();
-        });
-
-        if ($reset_password_status == Password::INVALID_TOKEN) {
-            return response()->json(["msg" => "Invalid token provided"], 400);
+            } else {
+                return response()->json(["error_msg" => 'User Not available.']);
+            }
+        } else {
+            return response()->json(["error_msg" => 'Token not match for this email.']);
         }
-
-        return response()->json(["msg" => "Password has been successfully changed"]);
     }
 }
