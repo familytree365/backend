cbcf34939655f26b8d18b0109c84bc4c63e6a4c4
Devdreamsolution <q3construction1@gmail.com>
2021-05-23 19:46:22 +0800

    Get user info with authcode

--- app/Http/Controllers/WikitreeController.php
+++ app/Http/Controllers/WikitreeController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class WikitreeController extends Controller
 {
     public function getAuthCode(Request $request)
     {
         $authCode = $request->authcode;
+        $endpoint = "https://api.wikitree.com/api.php";
+        $client = new \GuzzleHttp\Client();
 
-        return response()->json($authCode);
+        $response = $client->request('POST', $endpoint, ['query' => [
+            'authcode' => $authCode, 
+            'action' => 'clientLogin',
+            'format' => 'json'
+        ]]);
+
+        $statusCode = $response->getStatusCode();
+        $content = $response->getBody();
+        $userInfo = json_decode($response->getBody(), true);
+
+        return response()->json($userInfo);
     }
 }
